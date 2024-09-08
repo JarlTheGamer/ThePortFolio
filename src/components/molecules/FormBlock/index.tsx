86dc5f7b99ed9f09dd5619886da30eb3c81e6aa2
JarlTheGamer <103575351+JarlTheGamer@users.noreply.github.com>
2024-09-08 14:47:14 +0200

    Update index.tsx

--- src/components/molecules/FormBlock/index.tsx
+++ src/components/molecules/FormBlock/index.tsx
@@ -15,7 +15,7 @@ export default function FormBlock(props) {
     function handleSubmit(event) {
          event.preventDefault();
         try {
-            Status('pending');
+            status('pending');
             setError(null);
             const myForm = event.target;
             const formData = new FormData(myForm);
